2c8609c96453e990351bb441c5a909cf9f215932
Azmi ŞAHİN <6445951+azmisahin@users.noreply.github.com>
2024-10-01 22:41:31 +0000

    feat: create blockchain API

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
+// backend/src/index.js
 const express = require('express');
 const app = express();
-const port = 5000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello from Backend!');
+    res.send('Blockchain API is running');
 });
 
-app.listen(port, () => {
-    console.log(`Backend listening at http://localhost:${port}`);
+const PORT = process.env.BACKEND_PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
