c7ceeadfdd19f7cb7175184163d3aba85d235c44
Azmi ŞAHİN <6445951+azmisahin@users.noreply.github.com>
2024-10-02 18:32:56 +0000

    feat: Update Docker configurations and development environments
    - Updated .devcontainer configurations across various directories
    - Modified Dockerfiles for backend, blockchain, and frontend
    - Adjusted docker-compose.yml and .env files for better environment management
    - Removed obsolete .env and .gitignore files from backend and contracts
    - Added .dockerignore files to improve Docker build efficiency

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,11 +1,33 @@
+// blockchain/index.js
 const Web3 = require('web3');
+const dotenv = require('dotenv');
 
-// Local Ethereum node (e.g., Ganache)
-const web3 = new Web3('http://127.0.0.1:7545');
+// Ortam değişkenlerini yükle
+dotenv.config();
 
-async function main() {
+const WEB3_PROVIDER_DEFAULT_URL = 'http://ganache:8545';
+const WEB3_PROVIDER_URL = process.env.WEB3_PROVIDER_URL || WEB3_PROVIDER_DEFAULT_URL;
+const web3 = new Web3(WEB3_PROVIDER_URL);
+
+const main = async () => {
+    // Hesapları alın
     const accounts = await web3.eth.getAccounts();
-    console.log('Accounts:', accounts);
-}
 
-main();
+    console.log('Available Accounts:');
+    console.log(accounts);
+
+    // İlk hesabın bakiyesini kontrol et
+    const balance = await web3.eth.getBalance(accounts[0]);
+    console.log(`Account 1 Balance: ${web3.utils.fromWei(balance, 'ether')} ETH`);
+
+    // Basit bir işlem yapma (örnek olarak ilk hesaptan ikinciye transfer)
+    const tx = await web3.eth.sendTransaction({
+        from: accounts[0],
+        to: accounts[1],
+        value: web3.utils.toWei('1', 'ether')
+    });
+
+    console.log('Transaction:', tx);
+};
+
+main().catch(console.error);
